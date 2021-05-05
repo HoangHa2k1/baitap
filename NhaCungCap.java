64c778ca2198afc1451cb9c41cfc7f459ee1102c
HoangHa2k1 <78304800+HoangHa2k1@users.noreply.github.com>
2021-05-05 20:22:19 +0700

    Add files via upload

--- NhaCungCap.java
+++ NhaCungCap.java
@@ -1,10 +1,45 @@
 package Baitap;
-
+import java.util.Scanner;
 public class NhaCungCap {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main(String[] args){
+	protected String MaNhaCungCap;
+protected String DiaChi;
+public NhaCungCap() {
+	SetMaNhaCungCap ("");
+	DiaChi ="";
 }
+	
+public void SetMaNhaCungCap(String MaNhaCungCap)
+{
+	this.MaNhaCungCap= MaNhaCungCap;
+}
+public String getMaNhaCungCap()
+{
+	return MaNhaCungCap;
+}
+public void SetDiaChi(String DiaChi)
+{
+	this.DiaChi=DiaChi;
+}
+public String getDiaChi()
+{
+	return DiaChi;
+}
+public void nhap()
+{
+	try (Scanner sc = new Scanner(System.in)) {
+		System.out.print("Nhap Ma nha cung cap");
+		MaNhaCungCap = sc.nextLine();
+		SetMaNhaCungCap(MaNhaCungCap);
+		System.out.print("\nNhap dia chi nha cung cap: ");
+		DiaChi= sc.nextLine();
+	}
+	SetDiaChi(DiaChi);
+}
+public void Xuat()
+{
+	System.out.println("Ma nha cung cap : " + getMaNhaCungCap());
+	System.out.println("Dia chi : " +getDiaChi());
+}
+}
+
