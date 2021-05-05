769eeea62aaf8af2bcad5c327958d4e6889a8d89
HoangHa2k1 <78304800+HoangHa2k1@users.noreply.github.com>
2021-05-05 18:10:29 +0700

    Add files via upload

---
+++ ChiTietPhieuNhap.java
@@ -0,0 +1,35 @@
+package Baitap;
+	import java.util.Scanner;
+	public class ChiTietPhieuNhap extends PhieuNhap{
+		protected String MaSanPham;
+		protected int SoLuong;
+		protected int HanSuDung;
+		
+		public void setMaSanPham(String MaSanPham)
+		{
+			this.MaSanPham= MaSanPham;
+		}
+		public String getMaSanPham()
+		{
+			return MaSanPham;
+		}
+	    public void setSoLuong(int SoLuong )
+	    {
+	    	this.SoLuong= SoLuong;
+	    }
+	    public int getSoLuong()
+	    {
+	    	return SoLuong;
+	    }
+	    public void setHanSuDung(int HanSuDung)
+	    {
+	    	this.HanSuDung=HanSuDung;
+	    }
+	    public int getHanSuDung()
+	    {
+	    	return HanSuDung;
+	    }
+	    
+	    
+	}
+
---
+++ NhaCungCap.java
@@ -0,0 +1,10 @@
+package Baitap;
+
+public class NhaCungCap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
